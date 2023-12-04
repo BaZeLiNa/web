01fcf81331067dc4d0c72dbfcc22895f8435fbfe
Yurii <yura20xa@gmail.com>
2023-12-04 17:57:14 +0200

    adding lab 6 files

---
+++ react_lab/lab_6/src/Pages/Home.js
@@ -0,0 +1,56 @@
+import {ImageLanciaS4, ImageAudiS1, ImageLancia037} from "./images/Imports";
+import Heading from "../components/Heading/Heading";
+import {CardWrapper, ViewMore} from "./Home.style";
+import CardItem from "../components/CardItem/CardItem";
+import { Button } from "antd";
+
+const data = [
+    {
+      title: "Lancia delta S4",
+      text: `
+        The Lancia Delta S4, a standout in Group B rallying, 
+        is celebrated for its innovative turbocharged and supercharged engine design.`,
+      image: ImageLanciaS4,
+    },
+    {
+      title: "Audi Sport Quattro S1 E2",
+      text: `
+        The Audi Quattro S1 E2, a legend in Group B rallying, 
+        is renowned for its turbocharged power and distinctive 
+        5-cylinder engine design, maintaining performance without 
+        altering its length.`,
+      image: ImageAudiS1,
+    },
+    {
+      title: "Lancia 037",
+      text: `
+        The Lancia 037, a notable Group B rally car, 
+        is famed for its distinctive rear-wheel-drive and mid-engine 
+        setup, setting it apart in the iconic era of rallying.`,
+      image: ImageLancia037,
+    },
+];
+
+
+const Home = () => {
+  return (
+    <div>
+      <Heading/>
+      <CardWrapper>
+        {data.map(({ title, text, image}, idx) => (
+          <CardItem
+            title={title}
+            text={text}
+            imageSrc={image}
+            id={idx}
+          />
+        ))}
+      </CardWrapper>
+      <ViewMore>
+        <Button>View more</Button>
+      </ViewMore>
+    </div>
+  );
+};
+
+export default Home;
