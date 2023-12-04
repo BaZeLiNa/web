15a02bfc76a188aebb5d3db4dbed2fa1796b3ef8
Yurii <yura20xa@gmail.com>
2023-12-04 18:12:53 +0200

    adding lab 7

---
+++ react_lab/lab_7/src/Pages/Catalog/Catalog.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const CatalogWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: space-around;
+    width: 100%;
+    margin-top: 50px;
+`;
+
+export default CatalogWrapper;
